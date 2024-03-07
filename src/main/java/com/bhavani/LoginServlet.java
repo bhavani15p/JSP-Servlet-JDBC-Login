0a05a662ef33a38b9a1ef9d894bf24bb6297c277
Bhavani P <119038948+bhavani15p@users.noreply.github.com>
2024-03-07 14:41:52 +0530

    Add files via upload

---
+++ src/main/java/com/bhavani/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.bhavani;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            PrintWriter out = response.getWriter();
+            response.setContentType("text/html");
+            		
+            String u = request.getParameter("txtName");
+            String p = request.getParameter("txtPwd");
+
+           
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/loginPage", "root", "B#haVAni20!01");
+
+           
+            PreparedStatement ps = con.prepareStatement("select uname from login where uname=? and password=?");
+            ps.setString(1, u);
+            ps.setString(2, p);
+            ResultSet rs = ps.executeQuery();
+            
+	            if (rs.next()) {
+	                RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+	                rd.forward(request, response);
+	            } else {
+	                out.println("<font color=red size=18>Login failed!!<br>");
+	                out.println("<a href=login.jsp>Try Again!!");
+	            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
